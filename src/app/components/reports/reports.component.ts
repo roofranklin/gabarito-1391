74babea7abfd94b2e3d1cc276db0ced08950b9d9
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2025-08-09 12:22:37 -0300

    refatorando a maneira de prover serviços

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reports',
-  standalone: false,
+  standalone: true,
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.scss'
 })
